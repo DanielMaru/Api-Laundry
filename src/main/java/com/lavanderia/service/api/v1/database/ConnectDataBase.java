54546f2e26c0af535e064d8021cf2aeba049d9b1
Daniel Marulanda <danielfe95@hotmail.com>
2018-10-18 21:35:02 -0500

    Create dynamodb connection

--- src/main/java/com/lavanderia/service/api/v1/database/ConnectDataBase.java
+++ src/main/java/com/lavanderia/service/api/v1/database/ConnectDataBase.java
@@ -1,10 +1,26 @@
 package com.lavanderia.service.api.v1.database;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 
 public class ConnectDataBase {
 	
 	AmazonDynamoDB cliente;
+
+	public ConnectDataBase() {
+		cliente = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
+				new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+				.build(); 
+	}
+
+	public AmazonDynamoDB getCliente() {
+		return cliente;
+	}
+	
+	
+	
+	
 	
 	
 
--- src/test/java/com/lavanderia/service/api/v1/database/ConnectDataBaseTest.java
+++ src/test/java/com/lavanderia/service/api/v1/database/ConnectDataBaseTest.java
@@ -1,6 +1,23 @@
 package com.lavanderia.service.api.v1.database;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ConnectDataBaseTest {
 	
+	ConnectDataBase connectDataBase;
+	
+	@Before
+	public void initialize() {
+		
+	}
+	
+	@Test
+	public void instanceClient() {
+		connectDataBase = new ConnectDataBase();
+		assertNotNull(connectDataBase.getCliente());
+	}
 
 }
